679b4083c6efab284f81debe1fb3eb45a27eab12
Andrey Fontoura <andreyfontoura95@gmail.com>
2019-08-28 21:42:20 -0300

    corrected tags to show genre names

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import classes from './Movie.module.css';
 import MovieSummary from './MovieSummary/MovieSummary';
+//import genres from '../../genre_list.json';
 
 const movie = (props) => {
-  const {
-    src,
-    alt,
-    movieTitle,
-    overview,
-    popularity,
-    releaseDate,
-    tags } = props;
-
-  console.log(src);
+  const { src, id, movieTitle, overview, popularity, releaseDate, tags, click, genres } = props;
 
   return (
     <div className={classes.Movie}>
       <img
         src={`https://image.tmdb.org/t/p/w500/${src}`}
-        alt={alt}
+        alt={id}
         className={classes.Image} />
       <MovieSummary
         movieTitle={movieTitle}
         overview={overview}
         releaseDate={releaseDate}
         tags={tags}
-        popularity={popularity} />
+        popularity={popularity}
+        click={click}
+        tagNames={genres} />
     </div>
   );
 };
--- src/Components/Movie/MovieSummary/MovieSummary.js
+++ src/Components/Movie/MovieSummary/MovieSummary.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import classes from './MovieSummary.module.css';
 
-const movieInfo = (props) => (
-  <div className={classes.MovieInfo}>
-    <h2>{props.movieTitle}</h2>
-    <p>{props.overview}</p>
-    <button className={classes.tags}>{props.tags}</button>
-  </div>
-);
+const movieSummary = (props) => {
+  const { click, movieTitle, overview, tagNames, tags } = props;
+  return (
+    <div className={classes.MovieInfo}>
+      <h2 onClick={click}>{movieTitle}</h2>
+      <p>{overview}</p>
+      {tags.map(tag => (
+        <button
+          className={classes.tags}
+          key={tag}>
+          {tagNames[tag]}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default movieInfo;
+export default movieSummary;
